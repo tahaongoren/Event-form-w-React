8484e43a23ebb2c5782e5c862efa01057f2df7e6
tahaongoren <tahaongoren@gmail.com>
2022-02-02 10:47:02 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Todos</h1>
+      <Todo title="Learn JSX" text="okey sir" />
+      <Todo title="What is root" text="yeye"/>
+      <Todo title="Learn React" text="yeye"/>
     </div>
   );
 }
